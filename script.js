374f26be0bebe8ad36d65620cd66744c29074be8
261bayleyd <261bayley@gmail.com>
2024-11-14 17:59:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function run(){
-  let eoword = document.getElementById("inputuser").value
+  let eoinput = document.getElementById("inputuser").value
   let vowels = ["a", "e", "i", "o", "u"]
-  let firstletter = eoword.slice(0,1)
-  let rest = eoword.slice(1,eoword.length)
-  firstletter = firstletter.toLowerCase()
-  let pgword
-  if (vowels.includes(firstletter)){
-    pgword = rest+firstletter+"hay"
+  let eosplit = eoinput.split(/[\s,.!]+/)
+  for (let i = 0; i < eosplit.length; i++) {
+    if  (eosplit[i] != "") {
+      console.log(eosplit[i])
+      let firstletter = eosplit[i].slice(0,1)
+      let rest = eosplit[i].slice(1,eosplit[i].length)
+      firstletter = firstletter.toLowerCase()
+      console.log(firstletter)
+      let pgword
+      if (vowels.includes(firstletter)){
+        pgword = rest+firstletter+"hay"
+      }
+      else{
+        pgword = rest+firstletter+"ay"
+      }
+      eosplit[i] = pgword
+      console.log(pgword)
+    }
   }
-  else {
-    pgword = rest+firstletter+"ay"
-  }
-  document.getElementById("output").innerHTML=pgword
+  document.getElementById("output").innerHTML=eosplit.join(" ")
 }
